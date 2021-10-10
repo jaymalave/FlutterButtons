6bec4b170781113fb1df902c4b4b40e51dd1c509
Jay Malave <jaym.github@gmail.com>
2021-10-10 23:48:45 +0530

    added source code

---
+++ lib/src/glassmorphism_buttons.dart
@@ -0,0 +1,55 @@
+import "package:flutter/material.dart";
+
+class GlassyButton extends StatefulWidget {
+  const GlassyButton({
+    this.height,
+    this.width,
+    this.color1,
+    this.color2,
+    this.title,
+    this.splashColor,
+  });
+
+  final double? width;
+  final double? height;
+  final Color? color1;
+  final Color? color2;
+  final Color? splashColor;
+  final String? title;
+  @override
+  _GlassyButtonState createState() => _GlassyButtonState();
+}
+
+class _GlassyButtonState extends State<GlassyButton> {
+  Widget build(BuildContext build) {
+    return InkWell(
+      splashColor: widget.splashColor,
+      onTap: () {},
+      child: Container(
+        width: widget.width ?? MediaQuery.of(context).size.width * 0.25,
+        height: widget.height ?? MediaQuery.of(context).size.height * 0.05,
+        child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              '${widget.title}',
+              textAlign: TextAlign.center,
+            )),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              widget.color1 ?? const Color(0xcf90be).withOpacity(0.3),
+              widget.color2 ?? const Color(0xffc3f0).withOpacity(0.8),
+            ],
+            stops: [
+              0.5,
+              0.7,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
